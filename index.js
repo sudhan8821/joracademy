2b41d64d7c450ac72a66bd3fa1ba485941931e5d
sudhan8821 <87756790+sudhan8821@users.noreply.github.com>
2021-12-07 14:09:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,3 +7,19 @@ burger.addEventListener('click',()=>{
     nav_list.classList.toggle('v-class-resp');
     nav_bar.classList.toggle('h-nav-resp');
 })
+
+let phone=document.getElementById('phone');
+let perror=document.getElementById('perror');
+phone.addEventListener('keypress',(e)=>{
+    if(e.charCode<48 || e.charCode>75)
+    {
+        perror.innerHTML='Phone Number Can not be AlphaNumeric!';
+        perror.style.color='red';
+        perror.style.fontWeight='bold';
+        e.preventDefault();
+    }
+    else
+    {
+        perror.innerHTML='';
+    }
+})
